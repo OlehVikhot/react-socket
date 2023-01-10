27d4eac1bc91ceab6485a614bad3462b3f853986
Oleh Vikhot <vikhot57@gmail.com>
2023-01-10 14:47:07 +0200

    m

--- client/src/tests/TickerRow.test.js
+++ client/src/tests/TickerRow.test.js
@@ -1,12 +1,10 @@
 import { render, fireEvent, cleanup } from "@testing-library/react";
-import TickerRow from "../components/TickerTable/TickerRow/TickerRow";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import DragHandleIcon from "@mui/icons-material/DragHandle";
-
+import ReactRedux from "react-redux";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "../store/dataSlice";
+import { selectTicker } from "../store/dataSlice";
+import TickerRow from "../components/TickerTable/TickerRow/TickerRow";
 
 const store = configureStore({
   reducer: rootReducer,
@@ -59,4 +57,49 @@ test("displays correct direction icon based on change", () => {
     </Provider>
   ));
   expect(getByTestId("direction-icon")).toBeInTheDocument();
+});
+
+test("TickerRow component correctly renders data for each ticker", () => {
+  const data = {
+    ticker: "AAPL",
+    exchange: "NASDAQ",
+    price: "121.5",
+    change: "1.5",
+    change_percent: "1.25",
+    last_trade_time: "2022-05-20T16:00:00.000Z",
+  };
+
+  const { getByText } = render(
+    <Provider store={store}>
+      <TickerRow data={data} />
+    </Provider>
+  );
+
+  expect(getByText("AAPL")).toBeInTheDocument();
+  expect(getByText("NASDAQ")).toBeInTheDocument();
+  expect(getByText("121.5$")).toBeInTheDocument();
+  expect(getByText("1.5$")).toBeInTheDocument();
+  expect(getByText("1.25%")).toBeInTheDocument();
+  expect(getByText("May 20, 2022")).toBeInTheDocument();
+});
+
+jest.spyOn(ReactRedux, "useDispatch").mockImplementation(() => jest.fn());
+
+test('TickerRow component dispatches "selectTicker" action on star click', () => {
+  const data = {
+    ticker: "AAPL",
+    exchange: "NASDAQ",
+    price: "121.5",
+    change: "1.5",
+    change_percent: "1.25",
+    last_trade_time: "2022-05-20T16:00:00.000Z",
+  };
+
+  const dispatch = jest.fn();
+  jest.spyOn(ReactRedux, "useDispatch").mockImplementation(() => dispatch);
+
+  const { getByRole } = render(<TickerRow data={data} />);
+  const starIcon = getByRole("button", { name: /select ticker/i });
+  fireEvent.click(starIcon);
+  expect(dispatch).toHaveBeenCalledWith(selectTicker("AAPL"));
 });
